d2505fc9ddb2f77c3d5bb2fd9194b5f02d41e8ef
Alexey <lexuzieel@gmail.com>
2019-10-30 04:12:22 +0300

    Update install script

--- install.js
+++ install.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fs = require('fs')
 const crypto = require('crypto')
 
@@ -5,8 +6,11 @@ if (!fs.existsSync('.env')) {
     fs.copyFileSync('.env.example', '.env')
 }
 
-let key = crypto.createHash('sha256')
-    .update(crypto.randomBytes(256))
-    .digest("hex")
+if (!process.env.KEY) {
 
-fs.appendFileSync('.env', `\nKEY=${key}\n`)
+    let key = crypto.createHash('sha256')
+        .update(crypto.randomBytes(256))
+        .digest("hex")
+
+    fs.appendFileSync('.env', `\nKEY=${key}\n`)
+}
