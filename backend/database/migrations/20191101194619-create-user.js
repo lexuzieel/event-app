929bccbcf2018c14da47cdb164b26767194b2e88
Alexey Ivanov <lexuzieel@gmail.com>
2019-11-02 02:23:21 +0300

    Add user model

---
+++ backend/database/migrations/20191101194619-create-user.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
