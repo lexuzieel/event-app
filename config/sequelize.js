fdfd5842577fc87c9e0958f86e79b6ee61641be0
Alexey Ivanov <lexuzieel@gmail.com>
2019-11-02 00:06:21 +0300

    Add Sequelize ORM

---
+++ config/sequelize.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+let config = {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST || "127.0.0.1",
+    port: process.env.DATABASE_PORT || "3306",
+    dialect: process.env.DATABASE_DIALECT || "mysql",
+    dialectOptions: {
+        timezone: 'Etc/GMT+3',
+    },
+}
+
+module.exports = {
+    test: config,
+    production: config,
+    development: config,
+    use_env_variable: true,
+}
