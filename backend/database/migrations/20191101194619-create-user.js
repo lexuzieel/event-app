9ff525ca96e3717b33bc8b93c02c5b80983fe776
Alexey <lexuzieel@gmail.com>
2019-11-02 23:39:35 +0300

    Update migration format
    Make use of promise batch

--- backend/database/migrations/20191101194619-create-user.js
+++ backend/database/migrations/20191101194619-create-user.js
@@ -1,44 +1,51 @@
 'use strict';
+
+const table = 'users';
+
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('users', {
-            id: {
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-                type: Sequelize.INTEGER
-            },
-            profile_id: {
-                unique: true,
-                allowNull: false,
-                type: Sequelize.INTEGER
-            },
-            name: {
-                allowNull: false,
-                type: Sequelize.STRING
-            },
-            username: {
-                allowNull: true,
-                type: Sequelize.STRING
-            },
-            profile_url: {
-                type: Sequelize.STRING
-            },
-            avatar_url: {
-                allowNull: true,
-                type: Sequelize.STRING
-            },
-            created_at: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updated_at: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
+        return Promise.all([
+            queryInterface.createTable(table, {
+                id: {
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                    type: Sequelize.INTEGER
+                },
+                profile_id: {
+                    unique: true,
+                    allowNull: false,
+                    type: Sequelize.INTEGER
+                },
+                name: {
+                    allowNull: false,
+                    type: Sequelize.STRING
+                },
+                username: {
+                    allowNull: true,
+                    type: Sequelize.STRING
+                },
+                profile_url: {
+                    type: Sequelize.STRING
+                },
+                avatar_url: {
+                    allowNull: true,
+                    type: Sequelize.STRING
+                },
+                created_at: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updated_at: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                }
+            }),
+        ]);
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('users');
+        return Promise.all([
+            queryInterface.dropTable(table),
+        ]);
     }
 };
