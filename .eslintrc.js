c793a2bddc0cec750177cd9076007c26010a5bfe
Alexey <lexuzieel@gmail.com>
2019-11-04 04:15:18 +0300

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'vue/html-closing-bracket-spacing': 'off',
         'no-unused-vars': 'off',
         'no-undef': 'off',
         'import': 'off'
