5eee08ccccde68f09874dfe65f6964badd5d40c6
Alexey <lexuzieel@gmail.com>
2019-11-05 05:29:08 +0300

    Add Google analytics

--- frontend/index.js
+++ frontend/index.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import YandexMaps from 'vue-yandex-maps'
+import VueAnalytics from 'vue-analytics'
 
 window._ = require('lodash')
 window.axios = require('axios')
 
 Vue.config.productionTip = false
+if (process.env.VUE_APP_GOOGLE_ANALYTICS_ID) {
+    Vue.use(VueAnalytics, {
+        id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+        debug: {
+            sendHitTask: process.env.NODE_ENV == 'production',
+        }
+    })
+}
 
 const moment = require('moment')
 
